cec4865cdb6f94ef075e0d50e1860185c5a17c11
John Stout <john.j.stout.jr@gmail.com>
2023-10-03 13:39:07 -0400

    Updates

--- src/decode_lab_code/utils/signal_utils.py
+++ src/decode_lab_code/utils/signal_utils.py
@@ -10,6 +10,8 @@
 
 from scipy.signal import butter, sosfilt, lfilter
 import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
 
 class process_signal:
 
@@ -18,29 +20,92 @@ class process_signal:
     
     """
 
-    def __init__(self, data: float):
+    def __init__(self, data: float, fs: int):
 
         """
         Args:
             data: numpy array vector. Will be reshaped as needed.
+            fs: sampling rate as an integer
+
+            TODO: implement a way to detect if input is a list of signals
         """
 
         # tracking signal processing steps
         self.history = [] # list
 
-        # check the shape of the data and fix as needed
-        if len(data.shape) > 1:
+        # make sure we don't work with wrong kind of data
+        if type(data) is dict:
+            self.data = dict()
+            csc_ids = data.keys()
+            for csci in csc_ids:
+                if len(data[csci].shape) > 1:
+                    self.data[csci] = np.reshape(data[csci],len(data[csci]))
+            self.history.append("Reshaped data arrays to 1D")
 
-            # you have to reshape if you want any filtering to actually work
-            self.data = np.reshape(data,len(data))
+        # if just working with a single array, convert to dict
+        if type(data) is np.array:
+            # TODO: Make this compatible with dictionary type processing
 
-            # store a history of what you've done
-            self.history.append("Reshaped data array to 1D array")
+            # check the shape of the data and fix as needed
+            if len(data.shape) > 1:
 
-        else:
-            self.data = data
+                # you have to reshape if you want any filtering to actually work
+                self.data = np.reshape(data,len(data))
 
-    def butterworth_filter(self, lowcut: int, highcut:int, fs: int):
+                # store a history of what you've done
+                self.history.append("Reshaped data array to 1D array")
+
+            else:
+                self.data = data
+
+        # store sampling rate
+        self.fs = fs
+
+    def rereference(self, rereference_mode: str = 'CAR'):
+        """
+        This code supports common average or common median rereferencing techniques, which
+        are extremely useful techniques when working with a lot of recording channels from the same
+        structure
+
+        TODO: Implement an approach that supports matrix style or list style data inputs to self
+        
+        Args:
+            rereference_mode: Common average rereference, 'CAR' is default.
+                                Common medial rereference, 'CMR' is an option
+        
+        Returns:
+            self.data: rereferenced data
+
+        """
+        if 'CAR' in rereference_mode:
+            mode = 'common average'
+        elif 'CMR' in rereference_mode:
+            mode = 'common median'
+
+        # rereference each signal
+        if type(self.data) is dict:
+
+            # document history
+            self.history.append("signal_reref: rereferenced using the "+mode)
+
+            # reformat to np.array
+            csc_names = list(self.data.keys())
+            csc_array = np.zeros(shape=(len(self.data[csc_names[0]]),len(csc_names)))
+            for csci in range(len(csc_names)):
+                csc_array[:,csci] = self.data[csc_names[csci]]
+            
+            # collect the common '' rereference values
+            if 'CAR' in rereference_mode:
+                ref = np.mean(csc_array,axis=1)
+            elif 'CMR' in rereference_mode:
+                ref = np.median(csc_array,axis=1)
+
+            # rereference via subtraction and save output to self
+            self.signal_rereferenced = dict()
+            for i in range(csc_array.shape[1]):
+                self.signal_rereferenced[csc_names[i]] = csc_array[:,i]-ref
+
+    def butterworth_filter(self, data = None, lowcut = None, highcut = None):
 
         """
         Third degree butterworth filter
@@ -49,24 +114,62 @@ class process_signal:
             data: vector array of floating point numbers
             lowcut: lowcut for bandpass filtering
             highcut: highcut for pandpass filtering
-            fs: sampling rate
 
         Returns:
             signal_filtered: filtered signal
 
         """
 
-        # filter signal
-        nyq = 0.5 * fs # nyquist theorum
-        low = lowcut / nyq
-        high = highcut / nyq
-        order = 3 # third degree butterworth filter
+        # TODO: Make it so that this works on filtered or non-filtered signal
+        # [i for i in self.history if 'ref' in i]
 
-        # butter filter
-        sos = butter(N = order, Wn = [low,high], analog = False, btype='band', output='sos')
-        self.signal_filtered = sosfilt(sos, self.data)
-        resh = np.reshape(self.data, len(self.data))
-        filt = sosfilt(sos, resh)
+        if data is None:
+            data = self.data
 
+        if type(data) is dict:
+            self.signal_filtered = dict()
+            csc_ids = self.data.keys()
+            for csci in csc_ids:
+                #print(csci)
+                self.signal_filtered[csci] = butter_filt(data[csci],bandpass=[lowcut,highcut],fs=self.fs)
+                           
+        elif type(data) is np.array:
+            self.signal_filtered = butter_filt(data,bandpass=[lowcut,highcut],fs=self.fs)
+            
         # record
         self.history.append("signal_filtered: third degree butterworth filtering between "+str(lowcut)+" to "+str(highcut))
+
+# functions used for methods in object
+def butter_filt(signal: float, bandpass: list, fs: float):
+    """
+    butterworth filter
+    Args:
+        signal: input signal as a floating point variable
+        bandpass: list of bandpass filter, bandpass = [4, 12] for theta
+        fs: sampling rate
+
+    returns:
+        filt: filtered signal
+    """
+               
+    # parameter assignment for filtering
+    nyq = 0.5 * fs # nyquist theorum
+    low = bandpass[0] / nyq
+    high = bandpass[1] / nyq
+    order = 3 # third degree butterworth filter
+
+    # third degree butterworth filter
+    sos = butter(N = 3, Wn = [low,high], analog = False, btype='band', output='sos')
+    signal_filtered = sosfilt(sos, signal)
+
+    return signal_filtered 
+
+# TODO: a plot_signal class to quickly plot and visualize data
+
+# lets inherit the __init__ from process_signal
+class plot_signal(process_signal):
+    def multi_plotter(self, data, fs, ):
+        fig, axes = plt.subplots(nrows=len(data),ncols=1)
+        key_names = list(data.keys())
+        for i in range(len(data)):
+            axes[i].plot(data[key_names[i]][0:32000],'k',linewidth=1)
